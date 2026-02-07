97f956090e2c6676af815e36b164403067d3858b
Martin Wilck <martin_wilck@gmx.de>
2026-02-07 12:03:27 +0100

    bluej.sh: adapt java options to those use by the BlueJ installer

--- bluej.sh
+++ bluej.sh
@@ -3,5 +3,12 @@ APPBASE="/app/bluej"
 JAVAPATH="/app/jre"
 JAVAFX_LIB="/app/javafx/lib"
 CP="$APPBASE/lib/boot.jar"
-JAVAFX_CP=$(ls -1 /app/javafx/lib/*.jar | paste -sd ":" -)
-"$JAVAPATH/bin/java" -Djavafx.embed.singleThread=true -Dawt.useSystemAAFontSettings=on -cp "$CP:$JAVAFX_CP" bluej.Boot  "$@"
+JAVAFX_CP=$(ls -1 "$JAVAFX_LIB"/*.jar | paste -sd ":" -)
+
+export LD_LIBRARY_PATH="$JAVAFX_PATH/lib:$JAVAPATH/lib:/usr/lib64"
+"$JAVAPATH/bin/java" \
+    -Dhttps.protocols=TLSv1.2 \
+    -Dawt.useSystemAAFontSettings=on \
+    -cp "$CP:$JAVAFX_CP" \
+    bluej.Boot \
+    -javafxpath="$JAVAFXPATH" "$@"
